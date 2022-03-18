2bf1cd032d1175f349bfa20e21a3d06aba94203f
Amera Alleyne <amera.alleyne@gmail.com>
2022-03-18 19:18:47 +0000

    Use a while loop to repeat a chorus line

---
+++ chorus-while.js
@@ -0,0 +1,20 @@
+// while loop to print chorus 10 times
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+
+//while loop to change the key after five repetitions.
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
